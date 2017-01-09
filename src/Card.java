e2e6740fa946bda9f0f2a691a008a5883bf9f9a6
iamdann <d.soeten@gmail.com>
2017-01-09 18:25:27 +0100

    Added card names/values and put them in a LinkedHashMap

--- src/Card.java
+++ src/Card.java
@@ -1,4 +1,28 @@
 
 public class Card {
-
+	//De waarde van de kaart
+	private int value;
+	//De naam van de kaart
+	private String name;
+	
+	public Card(int value, String name){
+		this.setValue(value);
+		this.setName(name);
+	}
+	
+	public int getValue(){
+		return this.value;
+	}
+	
+	public void setValue(int value){
+		this.value = value;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
 }
--- src/Cards.java
+++ src/Cards.java
@@ -1,6 +1,26 @@
+import java.util.LinkedHashMap;
 
 public class Cards {
+	//kaartnamen en hun waardes
+	private String[][] cardnames = {
+			{"11", "KA"}, {"2", "K2"}, {"3", "K3"}, {"4", "K4"}, {"5", "K5"}, {"6", "K6"}, {"7", "K7"}, {"8", "K8"}, {"9", "K9"}, {"10", "K10"}, {"10", "KJ"}, {"10", "KQ"}, {"10", "KK"},
+			{"11", "SA"}, {"2", "S2"}, {"3", "S3"}, {"4", "S4"}, {"5", "S5"}, {"6", "S6"}, {"7", "S7"}, {"8", "S8"}, {"9", "S9"}, {"10", "S10"}, {"10", "SJ"}, {"10", "SQ"}, {"10", "SK"},
+			{"11", "HA"}, {"2", "H2"}, {"3", "H3"}, {"4", "H4"}, {"5", "H5"}, {"6", "H6"}, {"7", "H7"}, {"8", "H8"}, {"9", "H9"}, {"10", "H10"}, {"10", "HJ"}, {"10", "HQ"}, {"10", "HK"},
+			{"11", "RA"}, {"2", "R2"}, {"3", "R3"}, {"4", "R4"}, {"5", "R5"}, {"6", "R6"}, {"7", "R7"}, {"8", "R8"}, {"9", "R9"}, {"10", "R10"}, {"10", "RJ"}, {"10", "RQ"}, {"10", "RK"}
+			
+	};
+	
+	//Hierin worden alle kaartobjecten in opgeslagen
+	private LinkedHashMap<Integer, Card> cards = new LinkedHashMap<Integer, Card>();
+	
 	public Cards(){
-		Card card = new Card();
+		int counter = 0;
+		
+		for(int i = 0; i < 6; i++){
+			for(String[] card : this.cardnames){
+				this.cards.put(counter, new Card(Integer.parseInt(card[0]), card[1]));
+				counter++;
+			}
+		}
 	}
 }
